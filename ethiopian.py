12f453901077d3d08ae90c38d469c49981dc28b3
sehee-bang <i0475833@naver.com>
2023-01-12 09:51:52 +0900

    Feat:Getting two numbers
    사용자에게서 2개의 숫자값을 입력받는다 (정수로)

--- ethiopian.py
+++ ethiopian.py
@@ -1,5 +1,10 @@
-def hello():
-    print('Good luck!!')
+#Ethipian Multiply
 
-if __name__=='__main__':
-    hello()
+#사용자 값 받기
+m = int(input("숫자를 하나 입력하세요>> ")
+n = int(input("다른 숫자를 하나 더 입력하세요 >> ")
+
+result = 0
+
+print(m,n)
+
