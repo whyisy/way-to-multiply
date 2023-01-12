950ec396a36dc2478ecc9ce958c0699267903987
sehee-bang <i0475833@naver.com>
2023-01-12 09:56:52 +0900

    Feat:Adding condition
    홀수 일 때만 결과값에 더해지도록 조건문을 작성한다

--- ethiopian.py
+++ ethiopian.py
@@ -6,9 +6,11 @@ n = int(input("다른 숫자를 하나 더 입력하세요 >> ")
 
 result = 0
 
-print(m,n)
-
 #반복해서 왼쪽 값은 2로 나눈 후 몫을 구하고, 오른쪽 값은 2로 곱하기
 while m >= 1:
+    if m%2 != 0:
+        result += n
     m //= 2
     n *= 2
+
+print(result)
